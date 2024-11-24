bbecfaa95af47fe6773eb41e03c700f3ba536275
Mehmet Ali AKAR <maliakar123@gmail.com>
2024-11-24 12:08:18 +0300

    Layers created and Test Entity Added

---
+++ Core/ECommerceWebAPI.Application/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceWebAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceWebAPI.Application
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
---
+++ Core/ECommerceWebAPI.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceWebAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceWebAPI.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public double Price { get; set; }
+    }
+}
